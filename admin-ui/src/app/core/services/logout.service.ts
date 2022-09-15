bb0b7c0c31de0307ef07e45f7c60589413b5fe7a
Aaron Kili <aaronkilik@gmail.com>
2022-09-15 10:09:51 +0000

    add admin to baseurl

--- admin-ui/src/app/core/services/logout.service.ts
+++ admin-ui/src/app/core/services/logout.service.ts
@@ -23,7 +23,7 @@ export class LogoutService {
 
   logout() {
     this.http
-      .delete(`${this.appService.getConfig().baseUrl}${this.appService.getConfig().logout}`, {
+      .get(`${this.appService.getConfig().baseUrl}${this.appService.getConfig().logout}`, {
         observe: 'response'
       })
       .subscribe(
