0758b5384c8a45ee3076fe92a2ddd9412911dfe2
manzede benard <manzede@gmail.com>
2022-10-22 13:25:10 +0300

    Fix admin

--- admin-ui/src/app/shared/table/table.component.ts
+++ admin-ui/src/app/shared/table/table.component.ts
@@ -4,21 +4,21 @@ import {
   Input,
   OnChanges,
   Output,
-  EventEmitter
-} from '@angular/core';
-import { Router } from '@angular/router';
-import { SortModel } from 'src/app/core/models/sort.model';
-import { AppConfigService } from 'src/app/app-config.service';
-import * as appConstants from 'src/app/app.constants';
-import { CommonService } from 'src/app/core/services/common.service';
-import { TranslateService } from '@ngx-translate/core';
-import { AuditService } from 'src/app/core/services/audit.service';
-import { HeaderService } from 'src/app/core/services/header.service';
+  EventEmitter,
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { SortModel } from "src/app/core/models/sort.model";
+import { AppConfigService } from "src/app/app-config.service";
+import * as appConstants from "src/app/app.constants";
+import { CommonService } from "src/app/core/services/common.service";
+import { TranslateService } from "@ngx-translate/core";
+import { AuditService } from "src/app/core/services/audit.service";
+import { HeaderService } from "src/app/core/services/header.service";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.scss"],
 })
 export class TableComponent implements OnInit, OnChanges {
   @Input() data: [];
@@ -49,21 +49,22 @@ export class TableComponent implements OnInit, OnChanges {
     private auditService: AuditService
   ) {
     let lang = headerService.getUserPreferredLanguage();
-   translate.use(lang);
+    translate.use(lang);
   }
   ngOnInit(): void {
     this.tableData = [...this.data];
     this.sortStatusArray = [];
     this.lang = this.headerService.getUserPreferredLanguage();
-    const route = this.router.url.split('/')[3];
-    this.imageSource = appConstants.ListViewIdKeyMapping[`${route}`]['imagePath'];
+    const route = this.router.url.split("/")[3];
+    this.imageSource =
+      appConstants.ListViewIdKeyMapping[`${route}`]["imagePath"];
   }
 
   ngOnChanges(): void {
     this.tableData = [...this.data];
     this.columnsOfTableData = [];
-    this.displayedColumns.forEach(column => {
-      this.columnsOfTableData.push(column['name']);
+    this.displayedColumns.forEach((column) => {
+      this.columnsOfTableData.push(column["name"]);
     });
     this.setSortDirection();
   }
@@ -75,9 +76,9 @@ export class TableComponent implements OnInit, OnChanges {
           this.sortStatusArray.push(data.sortField);
         }
         const index = this.columnsOfTableData.indexOf(data.sortField);
-        if (data.sortType.toLowerCase() === 'asc') {
+        if (data.sortType.toLowerCase() === "asc") {
           this.sortIconTrackerArray[index] = 1;
-        } else if (data.sortType.toLowerCase() === 'desc') {
+        } else if (data.sortType.toLowerCase() === "desc") {
           this.sortIconTrackerArray[index] = -1;
         }
       });
@@ -85,19 +86,27 @@ export class TableComponent implements OnInit, OnChanges {
   }
 
   selectedRow(data: any, specData: any) {
-    let currentRouteType = this.router.url.split('/')[3];
+    let currentRouteType = this.router.url.split("/")[3];
     let id = null;
-    if(!currentRouteType.includes("view")){
+    if (!currentRouteType.includes("view")) {
       id = appConstants.ListViewIdKeyMapping[`${currentRouteType}`];
-    }else{
-      currentRouteType = this.router.url.split('/')[2];
+    } else {
+      currentRouteType = this.router.url.split("/")[2];
       id = appConstants.ListViewIdKeyMapping[`${currentRouteType}`];
     }
-    if (specData.callBackFunction && specData.callBackFunction !== '') {
-      if (currentRouteType.toLowerCase() === 'blocklisted-words') {
-        this.commonService[specData.callBackFunction]({...data}, specData.redirectURL +"$"+data.langCode, id.idKey);
-      }else{
-        this.commonService[specData.callBackFunction]({...data}, specData.redirectURL, id.idKey);
+    if (specData.callBackFunction && specData.callBackFunction !== "") {
+      if (currentRouteType.toLowerCase() === "blocklisted-words") {
+        this.commonService[specData.callBackFunction](
+          { ...data },
+          specData.redirectURL + "$" + data.langCode,
+          id.idKey
+        );
+      } else {
+        this.commonService[specData.callBackFunction](
+          { ...data },
+          specData.redirectURL,
+          id.idKey
+        );
       }
     }
   }
@@ -107,51 +116,51 @@ export class TableComponent implements OnInit, OnChanges {
   }
 
   getTableRowData(data: any, index: number, columnName: string) {
-    const routeIndex = this.router.url.lastIndexOf('/');
+    const routeIndex = this.router.url.lastIndexOf("/");
     this.currentRoute = this.router.url.slice(0, routeIndex);
-    const currentRouteType = this.router.url.split('/')[3];
+    const currentRouteType = this.router.url.split("/")[3];
     const id = appConstants.ListViewIdKeyMapping[`${currentRouteType}`];
     this.auditService.audit(7, id.auditEventId, currentRouteType);
     if (index === 0) {
-      if (currentRouteType.toLowerCase() === 'blocklisted-words') {
+      if (currentRouteType.toLowerCase() === "blocklisted-words") {
         this.router.navigate([
           `${this.currentRoute}/single-view`,
-          data[id.idKey] + '$' + data.langCode
+          data[id.idKey] + "$" + data.langCode,
         ]);
-      }else if (currentRouteType.toLowerCase() === 'dynamicfields') {
+      } else if (currentRouteType.toLowerCase() === "dynamicfields") {
         this.router.navigate([
           `${this.currentRoute}/single-view`,
-          data.fieldVal.code
+          data.fieldVal.code,
         ]);
       } else {
         this.router.navigate([
           `${this.currentRoute}/single-view`,
-          data[id.idKey]
+          data[id.idKey],
         ]);
       }
     }
   }
 
   sortColumn(columnName: string, columnIndex: number) {
-    this.auditService.audit(13, 'ADM-093', {
-      masterdataName: this.router.url.split('/')[3],
-      columnName
+    this.auditService.audit(13, "ADM-093", {
+      masterdataName: this.router.url.split("/")[3],
+      columnName,
     });
     const sortObject = this.sortData.filter(
-      data => data.sortField === columnName
+      (data) => data.sortField === columnName
     );
     let sortModel = new SortModel();
     if (sortObject.length === 0) {
       sortModel.sortField = columnName;
-      sortModel.sortType = 'asc';
+      sortModel.sortType = "asc";
       this.sortIconTrackerArray[columnIndex] = 1;
     } else {
       sortModel = sortObject[0];
-      if (sortModel.sortType.toLowerCase() === 'asc') {
-        sortModel.sortType = 'desc';
+      if (sortModel.sortType.toLowerCase() === "asc") {
+        sortModel.sortType = "desc";
         this.sortIconTrackerArray[columnIndex] = -1;
-      } else if (sortModel.sortType.toLowerCase() === 'desc') {
-        sortModel.sortType = 'asc';
+      } else if (sortModel.sortType.toLowerCase() === "desc") {
+        sortModel.sortType = "asc";
         this.sortIconTrackerArray[columnIndex] = 1;
       }
     }
@@ -167,81 +176,81 @@ export class TableComponent implements OnInit, OnChanges {
       if (!this.isUser) {
         // user mapping center menu options
         this.ellipsisList.filter((values, i) => {
-          values.filter(value => {
-            if (value.buttonName.eng === 'Activate') {
+          values.filter((value) => {
+            if (value.buttonName.eng === "Activate") {
               const index = this.ellipsisList[i].indexOf(value);
               this.ellipsisList[i].splice(index, 1);
             }
-          })
+          });
         });
-      } else{
-      this.ellipsisList.filter(values => {
-        if (values.buttonName.eng === 'Activate') {
-          const index = this.ellipsisList.indexOf(values);
-          this.ellipsisList.splice(index, 1);
-        }
-      });
-    }
+      } else {
+        this.ellipsisList.filter((values) => {
+          if (values.buttonName.eng === "Activate") {
+            const index = this.ellipsisList.indexOf(values);
+            this.ellipsisList.splice(index, 1);
+          }
+        });
+      }
     } else if (data.isActive === false) {
       // this.ellipsisList = [...this.buttonList];
       this.ellipsisList = JSON.parse(JSON.stringify(this.buttonList));
       if (!this.isUser) {
         // user mapping center menu options
         this.ellipsisList.filter((values, i) => {
-          values.filter(value => {
-            if (value.buttonName.eng === 'Deactivate') {
+          values.filter((value) => {
+            if (value.buttonName.eng === "Deactivate") {
               const index = this.ellipsisList[i].indexOf(value);
               this.ellipsisList[i].splice(index, 1);
             }
-          })
+          });
         });
-      } else{
-      this.ellipsisList.filter(values => {
-        if (values.buttonName.eng === 'Deactivate') {
-          const index = this.ellipsisList.indexOf(values);
-          this.ellipsisList.splice(index, 1);
-        }
-      });
-    }
-    }else{
+      } else {
+        this.ellipsisList.filter((values) => {
+          if (values.buttonName.eng === "Deactivate") {
+            const index = this.ellipsisList.indexOf(values);
+            this.ellipsisList.splice(index, 1);
+          }
+        });
+      }
+    } else {
       this.ellipsisList = [...this.buttonList];
     }
   }
 
   tableStyle(index, columnValue, columnName) {
     const myTableStyles = {
-      color: '#0F2126',
-      cursor: '',
-      border: '',
-      padding: '',
-      borderRadius: '',
-      backgroundColor: '',
-      whiteSpace: 'nowrap',
-      textTransform: '',
-      fontSize: ''
+      color: "#0F2126",
+      cursor: "",
+      border: "",
+      padding: "",
+      borderRadius: "",
+      backgroundColor: "",
+      whiteSpace: "nowrap",
+      textTransform: "",
+      fontSize: "",
     };
     if (index === 0) {
-      myTableStyles.color = '#0F2126';
-      myTableStyles.cursor = 'pointer';
+      myTableStyles.color = "#0F2126";
+      myTableStyles.cursor = "pointer";
       return myTableStyles;
     }
-    if (columnValue === true && columnName === 'isActive') {
-      myTableStyles.color = 'white';
-      myTableStyles.fontSize = '7.5px';
-      myTableStyles.backgroundColor = '#4AD991';
-      myTableStyles.padding = '4px 4px 4px 4px';
-      myTableStyles.border = '0.8px solid #68DA7E';
-      myTableStyles.borderRadius = '15px';
-      myTableStyles.textTransform = 'uppercase';
+    if (columnValue === true && columnName === "isActive") {
+      myTableStyles.color = "white";
+      myTableStyles.fontSize = "7.5px";
+      myTableStyles.backgroundColor = "#4AD991";
+      myTableStyles.padding = "4px 4px 4px 4px";
+      myTableStyles.border = "0.8px solid #68DA7E";
+      myTableStyles.borderRadius = "15px";
+      myTableStyles.textTransform = "uppercase";
       return myTableStyles;
-    } else if (columnValue === false && columnName === 'isActive') {
-      myTableStyles.color = 'white';
-      myTableStyles.backgroundColor = '#9C9F9F';
-      myTableStyles.fontSize = '7.5px';
-      myTableStyles.padding = '4px 4px 4px 4px';
-      myTableStyles.border = '0.8px solid #9C9F9F';
-      myTableStyles.borderRadius = '20px';
-      myTableStyles.textTransform = 'uppercase';
+    } else if (columnValue === false && columnName === "isActive") {
+      myTableStyles.color = "white";
+      myTableStyles.backgroundColor = "#9C9F9F";
+      myTableStyles.fontSize = "7.5px";
+      myTableStyles.padding = "4px 4px 4px 4px";
+      myTableStyles.border = "0.8px solid #9C9F9F";
+      myTableStyles.borderRadius = "20px";
+      myTableStyles.textTransform = "uppercase";
       return myTableStyles;
     }
   }
